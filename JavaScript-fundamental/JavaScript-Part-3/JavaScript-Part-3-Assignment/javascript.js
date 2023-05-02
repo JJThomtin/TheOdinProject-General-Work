d699d4e8689686e4f488afdaf40207fd80f467f4
Justin Thomas <129408043+JJThomtin@users.noreply.github.com>
2023-05-02 14:05:41 +0200

    Modified intex.html and style.css
    Code done for intex.html and style.css was for practice

---
+++ JavaScript-fundamental/JavaScript-Part-3/JavaScript-Part-3-Assignment/javascript.js
@@ -0,0 +1,15 @@
+function add7(x){
+    return x + 7;
+}
+
+function multiply(x, y){
+    return x*y;
+}
+
+function capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+function lastLetter(string){
+    return string.substr(-1);
+}
---
+++ JavaScript-fundamental/JavaScript-Part-3/javascript.js
@@ -0,0 +1,11 @@
+function printHelloWorld(){
+    console.log("Hello world!");
+}
+
+
+function addingNum(a, b){
+    console.log(`${a} + ${b} = ${a+b}`);
+}
+
+printHelloWorld();
+addingNum(4, 3)
